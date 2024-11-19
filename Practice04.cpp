119ad25d89e66548968313973d63348f05eef817
AbuBokkar95 <mahmud.nubtk@gmail.com>
2024-11-19 21:54:43 +0600

    Insertion at End & after a target value

---
+++ Practice04.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+};
+class Linkedlist{
+public:
+    Node *head;
+    Linkedlist(){
+        head = nullptr;
+    }
+    void insert_Head(int data){
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+    void insert_End(int data){
+        Node *newNode = new Node(data);
+        Node *temp = head;
+        while (temp->next!=nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    void insert_After(int data, int target){
+        Node *newNode = new Node(data);
+        Node *temp = head;
+        while (temp->data!=target)
+        {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        
+    }
+    void Print(){
+        Node *temp = head;
+        while (temp!=nullptr)
+        {
+            cout<<temp->data<<"->";
+            temp = temp->next;
+        }
+        cout<<"NULL";
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    Linkedlist list;
+
+    list.insert_Head(40);
+    list.insert_Head(20);
+    list.insert_Head(10);
+    list.insert_End(50);
+    list.Print();
+
+    list.insert_After(30, 20);
+    list.Print();
+    return 0;
+}
