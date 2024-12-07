1d249e9330ebd29e1bb19bc5275e810d0e7574a1
AbuBokkar95 <mahmud.nubtk@gmail.com>
2024-12-07 22:07:31 +0600

    deletion at middle

---
+++ Problem15.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+class Linkedlist
+{
+public:
+    Node *head;
+    Linkedlist()
+    {
+        head = nullptr;
+    }
+    void insert_Head(int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+    void delete_head(){
+        head = head->next;
+        head->prev = nullptr;
+    }
+    void delete_end(){
+        Node *t1 = head;
+        Node *t2 = head->next;
+        while (t2->next!=nullptr)
+        {
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        t1->next = nullptr;
+    }
+    // delete a middle node
+    void delete_Node(int target){
+        Node *t1 = head;
+        Node *t2 = head->next;
+        while (t2->data!=target)
+        {
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        t1->next = t2->next;
+        t2->next->prev = t1;
+    }
+    void Print()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << "<->";
+            temp = temp->next;
+        }
+        cout << "NULL";
+        cout << endl;
+    }
+    void ReversePrint(){
+        Node *temp = head;
+        while (temp->next !=nullptr)
+        {
+            temp = temp->next;
+        }
+        while (temp != nullptr)
+        {
+            cout << temp->data << "<->";
+            temp = temp->prev;
+        }
+        cout << "NULL";
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Linkedlist list;
+
+    list.insert_Head(50);
+    list.insert_Head(40);
+    list.insert_Head(30);
+    list.insert_Head(20);
+    list.insert_Head(10);
+
+    list.Print();
+
+    list.delete_Node(30);
+    list.Print();
+
+    list.ReversePrint();
+    return 0;
+}
